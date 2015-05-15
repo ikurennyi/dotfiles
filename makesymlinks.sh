7519d10c3f8532c159aa7908abfc281df1b567b5
Eugene Kurennyy <eugene.kurennyy@gmail.com>
2015-05-15 12:54:03 +0300

    Shuttle conf added to symlinked files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gemrc gitconfig gitignore zshrc"    # list of files/folders to symlink in homedir
+files="gemrc gitconfig gitignore shuttle.json zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
